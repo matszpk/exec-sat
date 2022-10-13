46b3ed6c5aac2bbb62ba6cc557a51715420ce327
matszpk <matszpk@interia.pl>
2022-10-13 21:54:42 +0200

    Add doc header.

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,10 @@
 //! The library to call SAT solver.
 //!
 //! This library provides routines to parse SAT solver output and to call SAT solver.
+//! It three contains functions:
+//! * `parse_sat_output` - to parse output from SAT solver after solving instance,
+//! * `call_sat_simple` - to call SAT solver program without arguments.
+//! * `call_sat` - to call SAT solver program with arguments.
 
 use std::ffi::OsStr;
 use std::io::{self, BufRead, BufReader, Read};
