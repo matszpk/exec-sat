69e6aaa884ca9339a2b13b65ca3923d7075909bb
matszpk <matszpk@interia.pl>
2025-02-16 08:42:41 +0100

    Fix typos in doc.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Exec-SAT
 
 This library provides routines to parse SAT solver output and to execute SAT solver.
-It three contains functions:
+It contains three functions:
 * `parse_sat_output` - to parse output from SAT solver after solving instance,
 * `exec_sat_simple` - to execute SAT solver program without arguments.
 * `exec_sat` - to execute SAT solver program with arguments.
--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@
 //! The library to execute SAT solver.
 //!
 //! This library provides routines to parse SAT solver output and to execute SAT solver.
-//! It three contains functions:
+//! It contains three functions:
 //! * `parse_sat_output` - to parse output from SAT solver after solving instance,
 //! * `exec_sat_simple` - to execute SAT solver program without arguments.
 //! * `exec_sat` - to execute SAT solver program with arguments.
